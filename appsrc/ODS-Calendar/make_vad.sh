dc94b6d74e09602704c4071fb87a65f66c10f93a
Debian Science Maintainers <debian-science-maintainers@lists.alioth.debian.org>
2021-07-28 00:15:17 +0100

    check_log_fix
    commit f78a4b20bd38ce064e62bc154dcca8a2109e015a
    Author: Maximiliano Curia <maxy@gnuservers.com.ar>
    Date:   Wed Jun 5 11:26:01 2013 +0200
    
        Check log fix
    
    
    Gbp-Pq: Name check_log_fix.patch

--- appsrc/ODS-Calendar/make_vad.sh
+++ appsrc/ODS-Calendar/make_vad.sh
@@ -447,7 +447,7 @@ chmod 644 ods_calendar_dav.vad
 chmod 644 ods_calendar_filesystem.vad
 
 CHECK_LOG
-RUN egrep  '"\*\*.*FAILED:|\*\*.*ABORTED:"' "$LOGFILE"
+RUN egrep -q '"\*\*.*FAILED:|\*\*.*ABORTED:"' "$LOGFILE"
 if test $STATUS -eq 0
 then
 	$myrm -f *.vad
