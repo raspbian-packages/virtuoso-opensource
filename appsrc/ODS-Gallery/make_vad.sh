d6c8115523e640d4dc99a1a1828161c574736df1
Debian Science Maintainers <debian-science-maintainers@lists.alioth.debian.org>
2021-10-20 22:11:44 +0100

    check_log_fix
    commit f78a4b20bd38ce064e62bc154dcca8a2109e015a
    Author: Maximiliano Curia <maxy@gnuservers.com.ar>
    Date:   Wed Jun 5 11:26:01 2013 +0200
    
        Check log fix
    
    
    Gbp-Pq: Name check_log_fix.patch

--- appsrc/ODS-Gallery/make_vad.sh
+++ appsrc/ODS-Gallery/make_vad.sh
@@ -436,7 +436,7 @@ STOP_SERVER
 chmod 644 ods_gallery_dav.vad
 
 CHECK_LOG
-RUN egrep  '"\*\*.*FAILED:|\*\*.*ABORTED:"' "$LOGFILE"
+RUN egrep -q '"\*\*.*FAILED:|\*\*.*ABORTED:"' "$LOGFILE"
 if test $STATUS -eq 0
 then
 	$myrm -f *.vad
