064dea7d66cad8bef4749656319878aaeb4b8d50
Sebastian Andrzej Siewior <sebastian@breakpoint.cc>
2023-02-08 15:53:21 +0000

    Fix FTBFS with openssl 1.1
    Bug-Debian: https://bugs.debian.org/828594
    Forwarded: https://github.com/openlink/virtuoso-opensource/pull/583
    Last-Update: Mo 11. Feb 13:50:17 CET 2019 (by Andreas Tille <tille@debian.org>)
    
    
    Gbp-Pq: Name ssl1.1.patch

--- libsrc/Dk/Dkernel.c
+++ libsrc/Dk/Dkernel.c
@@ -5280,7 +5280,11 @@ ssl_server_init ()
   }
 #endif
 
-  SSLeay_add_all_algorithms ();
+#if OPENSSL_VERSION_NUMBER < 0x10100000
+  SSLeay_add_all_algorithms();
+#else
+  OpenSSL_add_all_algorithms();
+#endif
   PKCS12_PBE_add ();		/* stub */
 
 #ifdef NO_THREAD
