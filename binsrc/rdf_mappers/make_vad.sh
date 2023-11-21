a6c72c1efc63ae32b5bd37cb6a55a7c92db1c886
Steven Chamberlain <steven@pyro.eu.org>
2023-11-21 02:29:04 +0100

    This patch avoids FTBFS due to wrong calculation of timeout
    If the current minute or second wraps around past 59, the timeout
    calculation is wrong, and may get stuck forever.  Since this happens
    in many parts of the testsuite it meant considerable risk of FTBFS.
    
    Gbp-Pq: Name safer-timeout.patch

--- binsrc/rdf_mappers/make_vad.sh
+++ binsrc/rdf_mappers/make_vad.sh
@@ -260,7 +260,7 @@ virtuoso_start() {
       "$SERVER" +wait
   fi
   stat="true"
-  while true
+  for i in $(seq 1 15)
   do
     sleep 4
     echo "Waiting Virtuoso Server start on port $PORT..."
@@ -271,17 +271,9 @@ virtuoso_start() {
       LOG "PASSED: Virtuoso Server successfully started on port $PORT"
       return 0
     fi
-    nowh=`date | cut -f 2 -d :`
-    nows=`date | cut -f 3 -d : | cut -f 1 -d " "`
-    nowh=`expr $nowh - $starth`
-    nows=`expr $nows - $starts`
-    nows=`expr $nows + $nowh \*  60`
-    if test $nows -ge $timeout
-    then
+  done
       LOG "***FAILED: Could not start Virtuoso Server within $timeout seconds"
       exit 1
-    fi
-  done
 }
 
 virtuoso_shutdown() {
--- binsrc/samples/sparql_demo/make_vad.sh
+++ binsrc/samples/sparql_demo/make_vad.sh
@@ -123,7 +123,7 @@ virtuoso_start() {
     fi
 
     stat="true"
-    while true
+    for i in $(seq 1 15)
     do
 	sleep 4
 	echo "Waiting $SERVER start on port $PORT..."
@@ -134,17 +134,9 @@ virtuoso_start() {
 	    LOG "PASSED: $SERVER successfully started on port $PORT"
 	    return 0
 	fi
-	nowh=`date | cut -f 2 -d :`
-	nows=`date | cut -f 3 -d : | cut -f 1 -d " "`
-	nowh=`expr $nowh - $starth`
-	nows=`expr $nows - $starts`
-	nows=`expr $nows + $nowh \*  60`
-	if test $nows -ge $timeout
-	then
+    done
 	    LOG "***FAILED: Could not start $SERVER within $timeout seconds"
 	    exit 1
-	fi
-    done
 }
 
 
