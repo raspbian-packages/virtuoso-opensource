06e571962b84feef7f09ab933267e55056aaddf6
Yavor Doganov <yavor@gnu.org>
2025-09-19 21:26:33 +0200

    Port to PCRE2.
    Bug-Debian: https://bugs.debian.org/999931
    Forwarded: no
    Last-Update: 2024-03-04
    
    Last-Update: 2024-03-04
    Gbp-Pq: Name pcre2.patch

--- libsrc/Wi/srvstat.c
+++ libsrc/Wi/srvstat.c
@@ -419,8 +419,8 @@ extern int dbf_log_fsync;
 extern int dbf_assert_on_malformed_data;
 extern int dbf_max_itc_samples;
 
-extern int32 c_pcre_match_limit;
-extern int32 c_pcre_match_limit_recursion;
+extern uint32 c_pcre_match_limit;
+extern uint32 c_pcre_match_limit_recursion;
 extern int32 pcre_max_cache_sz;
 extern int64 users_cache_sz;
 extern int32 enable_cpt_rb_ck;
